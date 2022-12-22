4b227ded5650313ad98a6a8c9c3ef0bc3d1c289e
Saqlaen <msaqlaenahmed98@gmail.com>
2022-12-22 08:50:13 +0530

    pizza bill

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,33 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int baseVeg;
+    private int baseNonveg;
+    private int extraCheese;
+    private int extraToppingVeg;
+    private int extrToppingNonVeg;
+    private int paperBag;
+    private boolean cheeseFlag;
+    private boolean toppingFlag;
+    private boolean paperBagFlag;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        bill = "";
+        baseVeg = 300;
+        baseNonveg = 400;
+        extraCheese = 80;
+        extraToppingVeg = 70;
+        extrToppingNonVeg = 120;
+        paperBag = 20;
+        
+        if( isVeg ){
+            this.price = baseVeg;
+        }
+        else{
+            this.price = baseNonveg;
+        }
+
     }
 
     public int getPrice(){
@@ -16,19 +39,52 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        cheeseFlag = true;
+        this.price += extraCheese;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        toppingFlag = true;
+        if( isVeg ){
+            this.price += extraToppingVeg;
+        }
+        else{
+            this.price += extrToppingNonVeg;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        paperBagFlag = true;
+        this.price += paperBag;
     }
 
     public String getBill(){
-        // your code goes here
+        if( isVeg ){
+            bill += "Base Price Of The Pizza: "+baseVeg+"\n";
+            if( cheeseFlag ){
+                bill += "Extra Cheese Added: "+extraCheese+"\n";
+            }
+            if( toppingFlag ){
+                bill += "Extra Toppings Added: "+extraToppingVeg+"\n";
+            }
+            if( paperBagFlag ){
+                bill += "Paperbag Added: "+paperBag+"\n";
+            }
+            bill += "Total Price: "+price+"\n";
+        }
+        else{
+            bill += "Base Price Of The Pizza: "+baseNonveg+"\n";
+            if( cheeseFlag ){
+                bill += "Extra Cheese Added: "+extraCheese+"\n";
+            }
+            if( toppingFlag ){
+                bill += "Extra Toppings Added: "+extrToppingNonVeg+"\n";
+            }
+            if( paperBagFlag ){
+                bill += "Paperbag Added: "+paperBag+"\n";
+            }
+            bill += "Total Price: "+price+"\n";
+        }
         return this.bill;
     }
 }
